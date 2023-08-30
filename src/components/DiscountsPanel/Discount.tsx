7a886bbcaa4e896dd513d4d4d515563d7eab3cb0
jkrahl <Jankrahls@gmail.com>
2023-08-30 23:25:38 +0200

    Title + Description added, link icon fixed

--- src/components/DiscountsPanel/Discount.tsx
+++ src/components/DiscountsPanel/Discount.tsx
@@ -19,17 +19,14 @@ export default function Discount({
     return (
         <div className={styles.discount} onClick={() => window.open(url)}>
             <div className={styles.discountImage}>
-                <img
-                    src={faviconUrl}
-                    alt="Logo"
-                    height={50}
-                    width={50}
-                />
+                <img src={faviconUrl} alt="Logo" height={50} width={50} />
             </div>
             <div className={styles.discountInfo}>
                 <div className={styles.discountTitle}>
                     <b>
-                        <span>{name} &#129109;</span>
+                        <span>
+                            {name}<ExternalIcon />
+                        </span>
                     </b>
                 </div>
                 <div className={styles.discountCategory}>
@@ -44,5 +41,26 @@ export default function Discount({
                 </div>
             </div>
         </div>
+    )
+}
+
+function ExternalIcon() {
+    return (
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M15.6396 7.02527H12.0181V5.02527H19.0181V12.0253H17.0181V8.47528L12.1042 13.3892L10.6899 11.975L15.6396 7.02527Z"
+                fill="currentColor"
+            />
+            <path
+                d="M10.9819 6.97473H4.98193V18.9747H16.9819V12.9747H14.9819V16.9747H6.98193V8.97473H10.9819V6.97473Z"
+                fill="currentColor"
+            />
+        </svg>
     )
 }
